9d0cacfd09bde4d1f6b7ee2f5ad1ec113ed36fba
Rjack00 <rjackson77@comcast.net>
2024-05-08 19:06:21 -0600

    created app.js with code to generate a pyramid in the console.

---
+++ app.js
@@ -0,0 +1,16 @@
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
